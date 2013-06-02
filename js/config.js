a463c0219dfda8499e9ce69b70ff3c1db02b6237
Mikhail Davydov <azazel.private@gmail.com>
2013-06-03 03:47:21 +0600

    repo paths update

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ var config = {
     character: {
         sprite_size: 64,
         sprite_scale: 0.5,
-        sprite_url: "images/characters.png"
+        sprite_url: "/images/characters.png"
     }
 };
 
